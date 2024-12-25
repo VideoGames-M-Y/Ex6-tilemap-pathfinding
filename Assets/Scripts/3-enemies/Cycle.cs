570b9e1d0d5ba35c4f9fe432852ff9b2d63bd249
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-25 10:33:51 +0200

    Update Cycle.cs

--- Assets/Scripts/3-enemies/Cycle.cs
+++ Assets/Scripts/3-enemies/Cycle.cs
@@ -1,17 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 /**
  * This component represents a cycle of points in space.
  * It can represent, for example, a patrol path for an AI.
  */
-public class Cycle : MonoBehaviour {
-    [SerializeField] float gizmoRadius = 0.2f;  // for gizmos of patrol points
+public class Cycle : MonoBehaviour
+{
+    [SerializeField] float gizmoRadius = 0.2f;  // Radius for gizmos of patrol points
 
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
-        for (int i = 0; i < transform.childCount; ++i) {
+
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            // Draw the sphere at each patrol point
             Gizmos.DrawSphere(transform.GetChild(i).position, gizmoRadius);
-            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild((i+1)% transform.childCount).position);
+
+            // Connect each point to the next one in the cycle
+            Gizmos.DrawLine(
+                transform.GetChild(i).position, 
+                transform.GetChild((i + 1) % transform.childCount).position
+            );
         }
     }
 }
