5b89fce2f43eff75061d3e1f65c8b1d33360ee7c
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-25 10:21:04 +0200

    Update ClickMover.cs

--- Assets/Scripts/2-player/ClickMover.cs
+++ Assets/Scripts/2-player/ClickMover.cs
@@ -1,19 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 /**
  * This component allows the player to move to a point in the screen by clicking it. 
  * Uses BFS to find the shortest path from the current location to the new location.
  */
-public class ClickMover: TargetMover {
-
+public class ClickMover : TargetMover
+{
     [SerializeField] InputAction moveTo = new InputAction(type: InputActionType.Button);
 
     [SerializeField]
     [Tooltip("Determine the location to 'moveTo'.")]
     InputAction moveToLocation = new InputAction(type: InputActionType.Value, expectedControlType: "Vector2");
 
-    void OnValidate() {
+    void OnValidate()
+    {
         // Provide default bindings for the input actions.
         // Based on answer by DMGregory: https://gamedev.stackexchange.com/a/205345/18261
         if (moveTo.bindings.Count == 0)
@@ -22,18 +23,22 @@ public class ClickMover: TargetMover {
             moveToLocation.AddBinding("<Mouse>/position");
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         moveTo.Enable();
         moveToLocation.Enable();
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         moveTo.Disable();
         moveToLocation.Disable();
     }
 
-    void Update() {
-        if (moveTo.WasPerformedThisFrame()) {
+    void Update()
+    {
+        if (moveTo.WasPerformedThisFrame())
+        {
             Vector3 newTarget = Camera.main.ScreenToWorldPoint(moveToLocation.ReadValue<Vector2>());
             SetTarget(newTarget);
         }
