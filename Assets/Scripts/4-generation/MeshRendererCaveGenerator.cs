ae0017387f8953111867ae7c2858cf4e4047c46a
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-25 10:37:34 +0200

    Update MeshRendererCaveGenerator.cs

--- Assets/Scripts/4-generation/MeshRendererCaveGenerator.cs
+++ Assets/Scripts/4-generation/MeshRendererCaveGenerator.cs
@@ -10,8 +10,8 @@ using UnityEngine;
  * Adapted by: Erel Segal-Halevi
  * Since: 2020-12
  */
-
-public class MeshRendererCaveGenerator: MonoBehaviour {
+public class MeshRendererCaveGenerator : MonoBehaviour
+{
     [SerializeField] MeshRenderer displayPlaneRenderer;
 
     [Tooltip("The percent of walls in the initial random map")]
@@ -29,58 +29,57 @@ public class MeshRendererCaveGenerator: MonoBehaviour {
 
     private CaveGenerator caveGenerator;
 
-    void Start() {
-        //To get the same random numbers each time we run the script
+    void Start()
+    {
+        // To get the same random numbers each time we run the script
         Random.InitState(100);
 
         caveGenerator = new CaveGenerator(randomFillPercent, gridSize);
         caveGenerator.RandomizeMap();
 
-        //For testing that init is working
+        // For testing that init is working
         GenerateAndDisplayTexture(caveGenerator.GetMap());
 
-        //Start the simulation
+        // Start the simulation
         SimulateCavePattern();
     }
 
-
-    //Do the simulation in a coroutine so we can pause and see what's going on
-    async void SimulateCavePattern() {
-        for (int i = 0; i < simulationSteps; i++) {
+    // Do the simulation in a coroutine so we can pause and see what's going on
+    async void SimulateCavePattern()
+    {
+        for (int i = 0; i < simulationSteps; i++)
+        {
             await Awaitable.WaitForSecondsAsync(pauseTime);
 
-            //Calculate the new values
+            // Calculate the new values
             caveGenerator.SmoothMap();
 
-            //Generate texture and display it on the plane
+            // Generate texture and display it on the plane
             GenerateAndDisplayTexture(caveGenerator.GetMap());
         }
         Debug.Log("Simulation completed!");
     }
 
-
-
-    //Generate a black or white texture depending on if the pixel is cave or wall
-    //Display the texture on a plane
-    private void GenerateAndDisplayTexture(int[,] data) {
-        //We are constantly creating new textures, so we have to delete old textures or the memory will keep increasing
-        //The garbage collector is not collecting unused textures
+    // Generate a black or white texture depending on if the pixel is cave or wall
+    // Display the texture on a plane
+    private void GenerateAndDisplayTexture(int[,] data)
+    {
+        // We are constantly creating new textures, so we have to delete old textures or the memory will keep increasing
+        // The garbage collector is not collecting unused textures
         Resources.UnloadUnusedAssets();
-        //We could also use 
-        //Destroy(displayPlaneRenderer.sharedMaterial.mainTexture);
-        //Or reuse the same texture
 
-
-        //These two arrays are always the same so we could init them once at start
+        // These two arrays are always the same so we could init them once at start
         Texture2D texture = new Texture2D(gridSize, gridSize);
 
         texture.filterMode = FilterMode.Point;
 
         Color[] textureColors = new Color[gridSize * gridSize];
 
-        for (int y = 0; y < gridSize; y++) {
-            for (int x = 0; x < gridSize; x++) {
-                //From 2d array to 1d array
+        for (int y = 0; y < gridSize; y++)
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                // From 2d array to 1d array
                 textureColors[y * gridSize + x] = data[x, y] == 1 ? Color.black : Color.white;
             }
         }
