a1a9b6e3e6c03b0c6bef4b8e20b6fb04f6df1ee0
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-25 10:18:51 +0200

    Update Patroller.cs

--- Assets/Scripts/3-enemies/Patroller.cs
+++ Assets/Scripts/3-enemies/Patroller.cs
@@ -1,22 +1,26 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 /**
  * This component patrols between given points.
  */
-public class Patroller: TargetMover {
+public class Patroller : TargetMover
+{
     [SerializeField] Cycle patrolPath = null;
 
     [SerializeField] private int pointCount;
     [SerializeField] private int currentPointIndex;
 
-    protected override void Start()  {
+    protected override void Start()
+    {
         base.Start();
         pointCount = patrolPath.transform.childCount;
         currentPointIndex = 0;
     }
 
-    private void Update() {
-        if (atTarget) {
+    private void Update()
+    {
+        if (atTarget)
+        {
             currentPointIndex = (currentPointIndex + 1) % pointCount;
         }
         SetTarget(patrolPath.transform.GetChild(currentPointIndex).position);
