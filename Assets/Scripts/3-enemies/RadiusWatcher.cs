ad3cc7dd22562a3ace72307438f6329bfe0b91a4
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-25 10:34:26 +0200

    Update RadiusWatcher.cs

--- Assets/Scripts/3-enemies/RadiusWatcher.cs
+++ Assets/Scripts/3-enemies/RadiusWatcher.cs
@@ -1,30 +1,40 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 /**
- * This component makes its object watch a given radius, and if the target is found - it starts chasing it.
+ * This component makes its object watch a given radius, and if the target is found, it starts chasing it.
  */
 [RequireComponent(typeof(Chaser))]
-public class RadiusWatcher: MonoBehaviour {
+public class RadiusWatcher : MonoBehaviour
+{
     [SerializeField] float radiusToWatch = 5f;
 
     private Chaser chaser;
-    private void Start() {
+
+    private void Start()
+    {
         chaser = GetComponent<Chaser>();
         chaser.enabled = false;
     }
 
-    void Update() {
+    void Update()
+    {
         float distanceToTarget = Vector3.Distance(
             transform.position, chaser.TargetObjectPosition());
-        if (distanceToTarget <= radiusToWatch) {
+
+        if (distanceToTarget <= radiusToWatch)
+        {
             chaser.enabled = true;
-        } else {
+        }
+        else
+        {
             chaser.enabled = false;
         }
     }
 
-    private void OnDrawGizmosSelected() {
-        if (enabled) {
+    private void OnDrawGizmosSelected()
+    {
+        if (enabled)
+        {
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position, radiusToWatch);
         }
