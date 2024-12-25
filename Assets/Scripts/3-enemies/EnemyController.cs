aa1a276fb336c56269718f79413f8988bf0c59ca
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-25 10:06:28 +0200

    Update EnemyController.cs

--- Assets/Scripts/3-enemies/EnemyController.cs
+++ Assets/Scripts/3-enemies/EnemyController.cs
@@ -1,36 +1,43 @@
-﻿﻿using UnityEngine;
+﻿using UnityEngine;
 
 /**
  * This component patrols between given points, and chases a given target object when it sees it.
  */
 [RequireComponent(typeof(Patroller))]
 [RequireComponent(typeof(Chaser))]
-public class EnemyController: MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
     [SerializeField] float radiusToWatch = 5f;
 
     private Chaser chaser;
     private Patroller patroller;
-    private void Start() {
+
+    private void Start()
+    {
         chaser = GetComponent<Chaser>();
         patroller = GetComponent<Patroller>();
         chaser.enabled = false;
         patroller.enabled = true;
     }
 
-    private void Update() {
+    private void Update()
+    {
         float distanceToTarget = Vector3.Distance(transform.position, chaser.TargetObjectPosition());
-        if (distanceToTarget <= radiusToWatch) {
+        if (distanceToTarget <= radiusToWatch)
+        {
             chaser.enabled = true;
             patroller.enabled = false;
-        } else {
+        }
+        else
+        {
             patroller.enabled = true;
             chaser.enabled = false;
         }
     }
 
-    private void OnDrawGizmosSelected() {
+    private void OnDrawGizmosSelected()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, radiusToWatch);
     }
 }
- 
