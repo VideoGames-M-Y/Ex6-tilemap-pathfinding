1bcfa137e1f0992e5226948baa59722de1e6e61d
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-25 10:25:42 +0200

    Update AllowedTiles.cs

--- Assets/Scripts/1-tiles/AllowedTiles.cs
+++ Assets/Scripts/1-tiles/AllowedTiles.cs
@@ -1,21 +1,26 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class AllowedTiles : MonoBehaviour {
+public class AllowedTiles : MonoBehaviour
+{
     [SerializeField] private List<TileBase> allowedTiles = new List<TileBase>(); // Use List instead of array
 
-    public bool Contains(TileBase tile) {
+    public bool Contains(TileBase tile)
+    {
         return allowedTiles.Contains(tile); // Works with List
     }
 
-    public void AddTile(TileBase tile) {
-        if (!allowedTiles.Contains(tile)) {
+    public void AddTile(TileBase tile)
+    {
+        if (!allowedTiles.Contains(tile))
+        {
             allowedTiles.Add(tile); // Add the tile to the list
         }
     }
 
-    public TileBase[] Get() {
+    public TileBase[] Get()
+    {
         return allowedTiles.ToArray();  // Convert List to an array
     }
 }
